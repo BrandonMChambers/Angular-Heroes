97f33d25412ec32870e11018302236cc9a0826e3
Brandon Chambers <brandonmchambers36@gmail.com>
2020-01-12 17:06:13 -0500

    first commit section 3

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero [] =  [
+
+    {id: 11, name: 'Naruto'},
+    {id: 12, name: 'Sasuke'},
+    {id: 13, name: 'One Punch Man'},
+    {id: 14, name: 'Itachi'},
+    {id: 15, name: 'Goku'},
+    {id: 16, name: 'Piccolo'},
+    {id: 17, name: 'Luffy'},
+    {id: 18, name: 'Zoro'},
+    {id: 19, name: 'Gara'},
+    {id: 20, name: '9 Tails'}
+];
